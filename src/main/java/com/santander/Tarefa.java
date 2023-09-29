6651b1ce2e149b18e9a08db9667728ea223cff69
Jhonathan <jhonathanfeijomoraes@gmail.com>
2023-09-29 12:45:07 -0400

    List do bootcamp Santander

---
+++ src/main/java/com/santander/Tarefa.java
@@ -0,0 +1,13 @@
+package com.santander;
+
+public class Tarefa {
+
+    public Tarefa(String descrioao){
+        this.descricao = descrioao;
+    }
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
---
+++ src/main/java/com/santander/list/CarrinhoDeCompras.java
@@ -0,0 +1,48 @@
+package com.santander.list;
+
+import com.santander.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itens;
+
+    public CarrinhoDeCompras(){
+        itens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        itens.add(new Item(nome, preco, quantidade));
+    }
+    public void removerItem(String nome){
+        List<Item> itensRemovidos = new ArrayList<>();
+        for(Item item : itens) {
+            if(item.getNome().equalsIgnoreCase(nome))
+                itensRemovidos.add(item);
+        }
+        itens.removeAll(itensRemovidos);
+    }
+    public double calcularValorTotal(){
+        double valorTotal = 0;
+        for (Item item : itens)
+            valorTotal += item.getPreco() * item.getQuantidade();
+        return valorTotal;
+    }
+
+    public void exibirItens(){
+        System.out.println(itens);
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+        carrinho.adicionarItem("Telefone", 20.00, 5);
+        carrinho.adicionarItem("Computador", 15.75, 7);
+        System.out.println(carrinho.calcularValorTotal());
+        carrinho.exibirItens();
+        carrinho.removerItem("computador");
+        carrinho.exibirItens();
+        System.out.println(carrinho.calcularValorTotal());
+    }
+}
